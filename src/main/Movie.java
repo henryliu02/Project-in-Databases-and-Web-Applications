8b659fa733d435c009a922148f5428b577a999f5
lisazhanglii <2010295336@qq.com>
2023-05-11 22:46:21 -0700

    add the movie sax parser(print only)

---
+++ src/main/Movie.java
@@ -0,0 +1,66 @@
+package main;
+
+public class Movie {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    private String id;
+    private String title;
+    private int year;
+    private String director;
+    public Movie(){
+
+    }
+    public Movie(String id, String title, int year, String director) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.director = director;
+    }
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Movie Details - ");
+        sb.append("ID: ").append(id);
+        sb.append(", ");
+        sb.append("Title: ").append(title);
+        sb.append(", ");
+        sb.append("Year: ").append(year);
+        sb.append(", ");
+        sb.append("Director: ").append(director);
+        sb.append(".");
+
+        return sb.toString();
+    }
+}
+
+
+
