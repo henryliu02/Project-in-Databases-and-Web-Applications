a3c185c6fd554e6a935981b4296f70eb9673aebe
Henry(Ziheng) LIU <zihengl6@uci.edu>
2023-06-06 22:45:15 -0700

    master-slave mysql routing updated, updated testing mode recaptcha.

--- src/RecaptchaConstants.java
+++ src/RecaptchaConstants.java
@@ -1,5 +1,6 @@
 
 public class RecaptchaConstants {
-    public static final String SECRET_KEY ="6LcnUe4lAAAAAJ6PrTcf7EITkGQ35kwosWkVFxpW";
+//    public static final String SECRET_KEY ="6LcnUe4lAAAAAJ6PrTcf7EITkGQ35kwosWkVFxpW";
+    public static final String SECRET_KEY = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"; // testing mode
 
 }
